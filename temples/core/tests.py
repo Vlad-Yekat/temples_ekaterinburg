bb6955aa162b2d052d470ba56ff67e9a2cdd59ae
Vladislav Rybin <vladislavi@mail.ru>
2020-07-14 14:49:44 +0500

    2

--- temples/core/tests.py
+++ temples/core/tests.py
@@ -1,5 +1,6 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from core.views import index
 
 
@@ -7,3 +8,10 @@ class IndexPageTest(TestCase):
     def test_root_url(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+
+    def test_root_page_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertContains(response, '<title>Храмы Екатеринбурга</title>')
+        self.assertTrue(response.content.endswith(b'</html>'))
--- temples/core/views.py
+++ temples/core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h1>Test</h1>')
+    return HttpResponse('<html><title>Храмы Екатеринбурга</title></html>')
