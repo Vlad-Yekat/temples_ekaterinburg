96fc34f542eaecce1710d3a28bb5dcc282993b9f
Vladislav Rybin <vladislavi@mail.ru>
2020-07-14 09:20:10 +0500

    ...

--- temples/tdd_goat_test.py
+++ temples/tdd_goat_test.py
@@ -1,20 +1,25 @@
 """
-TDD
+TDD functional test first
 """
+import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def main_test():
-    """
-    TDD
-    :return:
-    """
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    browser.get('http://127.0.0.1:8080')
+class MainTest(unittest.TestCase):
 
-    assert 'framework' in browser.title
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_start_page(self):
+        self.browser.get('http://127.0.0.1:8080')
+
+        assert 'Храмы' in self.browser.title
 
 
 if __name__ == '__main__':
-    main_test()
+    unittest.main(warnings='ignore')
