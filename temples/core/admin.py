dd6c26898441014c31e2c07eaa846e384e450ca0
Vladislav Rybin <vladislavi@mail.ru>
2020-08-14 09:59:50 +0500

    140820

--- temples/core/admin.py
+++ temples/core/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.http import urlencode
 from .models import Church, City, Country, Address, Tag, District, Street, Comment
 
-admin.site.register(Church)
+
+class CustomChurch(admin.ModelAdmin):
+    list_display = ("main_order", "name", "slug", "short_description")
+    search_fields = ("short_description__contains", "name__contains", "slug__contains", )
+
+
+admin.site.register(Church, CustomChurch)
+
 admin.site.register(City)
 
 
@@ -12,11 +21,28 @@ class CustomCountry(admin.ModelAdmin):
 
 admin.site.register(Country, CustomCountry)
 
-admin.site.register(Address)
+
+class CustomAddress(admin.ModelAdmin):
+    list_display = ("address_str", "city", "district")
+    list_filter = ("city", )
+
+
+admin.site.register(Address, CustomAddress)
 
 
 class CustomDistrict(admin.ModelAdmin):
-    list_display = ("name", "slug")
+    list_display = ("name", "slug", "list_churches")
+
+    def list_churches(self, obj):
+        count = obj.address_set.count()
+        url = (
+                reverse("admin:core_address_changelist")
+                + "?"
+                + urlencode({"district__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{} object(s)</a>', url, count)
+
+    list_churches.short_description = "Churches"
 
 
 admin.site.register(District, CustomDistrict)
@@ -31,8 +57,8 @@ class CustomTag(admin.ModelAdmin):
         return format_html("<b><i>{}</i></b>", result)
 
     def count_churches(self, obj):
-        result = Church.objects.filter(tag=obj).count()
-        return result
+        count = Church.objects.filter(tag=obj).count()
+        return count
 
     count_churches.short_description = "Сhurch count"
 
