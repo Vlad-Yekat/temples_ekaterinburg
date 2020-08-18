045e380dc65bc1be9a607f12be44ec5e20e55106
Vladislav Rybin <vladislavi@mail.ru>
2020-08-18 14:43:39 +0500

    180820

--- temples/core/admin.py
+++ temples/core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.urls import reverse
 from django.utils.http import urlencode
+from django import forms
 from .models import Church, City, Country, Address, Tag, District, Street, Comment
 
 
@@ -12,7 +13,16 @@ class CustomChurch(admin.ModelAdmin):
 
 admin.site.register(Church, CustomChurch)
 
-admin.site.register(City)
+
+class CustomCity(admin.ModelAdmin):
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields["name"].label = "Name of City or small town in agglomeration:"
+        return form
+
+
+admin.site.register(City, CustomCity)
 
 
 class CustomCountry(admin.ModelAdmin):
@@ -46,7 +56,25 @@ class CustomDistrict(admin.ModelAdmin):
 
 
 admin.site.register(District, CustomDistrict)
-admin.site.register(Street)
+
+
+class CustomStreetForm(forms.ModelForm):
+    class Meta:
+        model = Street
+        fields = "__all__"
+
+    def clean_name(self):
+        if self.cleaned_data["name"].startswith('ул'):
+            raise forms.ValidationError("Enter only street name")
+
+        return self.cleaned_data["name"]
+
+
+class CustomStreet(admin.ModelAdmin):
+    form = CustomStreetForm
+
+
+admin.site.register(Street, CustomStreet)
 
 
 class CustomTag(admin.ModelAdmin):
