997a16e59c65a77223979e3b617238898b5f1bd8
Vladislav Rybin <vladislavi@mail.ru>
2020-08-18 15:19:53 +0500

    180820 jsonb

--- temples/users/models.py
+++ temples/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import JSONField
 
 
 class CustomUser(AbstractUser):
-    pass
+    notifications = JSONField(default={})
