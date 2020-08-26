0ce16de81625ed41ec95f03101597e148f462eeb
Vladislav Rybin <vladislavi@mail.ru>
2020-08-26 10:12:24 +0500

    Add open-source free template that will work on pc and mobile

--- temples/temples/settings.py
+++ temples/temples/settings.py
@@ -149,5 +149,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    "/home/vlad/my_projects/hramy/temples_ekaterinburg/temples/static",
+]
