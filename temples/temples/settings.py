803e73375b8e6e57a749bc2344ce67739879eae3
Vladislav Rybin <vladislavi@mail.ru>
2021-01-01 14:36:11 +0500

    Add swagger OpenAPI3

--- temples/temples/settings.py
+++ temples/temples/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'core.apps.CoreConfig',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
