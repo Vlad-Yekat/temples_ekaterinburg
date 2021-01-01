cd792fe09f1b04c57d455db0cc161f6c99ff4e8b
Vladislav Rybin <vladislavi@mail.ru>
2021-01-01 14:21:32 +0500

    Fix changes 010121

--- temples/temples/settings.py
+++ temples/temples/settings.py
@@ -55,12 +55,14 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
     'rest_framework',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -69,6 +71,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'temples.urls'
+
+CORS_ALLOWED_ORIGINS = []
 
 TEMPLATES = [
     {
